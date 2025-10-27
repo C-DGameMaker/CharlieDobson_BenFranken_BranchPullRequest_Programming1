77ed8230de1ac860b7a7d877c7c55722f515f406
C-DGameMaker <charlotte5634@outlook.com>
2025-10-27 09:29:25 -0300

    Made Some Changes

--- CharlieDobson_BenFranken_BranchPullRequest_Programming1/Program.cs
+++ CharlieDobson_BenFranken_BranchPullRequest_Programming1/Program.cs
@@ -11,11 +11,16 @@ namespace CharlieDobson_BenFranken_BranchPullRequest_Programming1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
             Console.ReadKey(true);
             Console.Clear();
 
             Console.WriteLine("This is our other branch speaking.");
+            Console.ReadKey(true);
+            Console.Clear();
+
+            Console.WriteLine("Now were gonna do the super cool thing of run into some issues but it's okay cause we can fix it.");
+            Console.ReadKey(true);
+            Console.Clear();
         }
     }
 }
